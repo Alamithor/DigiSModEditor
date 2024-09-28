df78607b68d1b166acf6a13de00b30ccd307d227
Alamithor <ahmad.yudhistira.alam@gmail.com>
2024-09-28 21:33:57 +0700

    add docstring in core.py

--- src/DigiSModEditor/core.py
+++ src/DigiSModEditor/core.py
@@ -18,6 +18,21 @@ log = logging.getLogger(const.LogName.MAIN)
 
 
 def get_asset_related_files(asset_name, files_text) -> Dict:
+    """
+    Given an asset name and a text containing all files in a directory, returns a dictionary
+    containing related files for the asset. The returned dictionary will have the following structure:
+    {
+        'asset_name': {
+            'Name': [<asset_name>],
+            'Geometry': [<asset_name>.geom],
+            'Skeleton': [<asset_name>.skel],
+            'Animation': [sorted list of <asset_name>_<number>.anim]
+        }
+    }
+    :param asset_name: The name of the asset
+    :param files_text: A text containing all files in the directory
+    :return: A dictionary containing related files for the asset
+    """
     file_name, ext = os.path.splitext(asset_name)
     # r_geo = f'({file_name})' + const.Pattern.GEO
     # r_skel = f'({file_name})' + const.Pattern.SKEL
@@ -42,6 +57,19 @@ def get_asset_related_files(asset_name, files_text) -> Dict:
 
 
 def create_project_mods_structure(project_name: str, dir_path: Union[PathLike, Path]) -> Path:
+    """
+    Creates a directory structure for a project mods.
+
+    Given a project name and a directory path, creates a directory structure for the project
+    mods. The directory structure will be the following:
+    - Project directory
+        - modfiles (directory)
+
+    :param project_name: The name of the project
+    :param dir_path: The directory path where the project directory will be created
+    :return: The path of the project directory
+    :raises ValueError: If the project name is empty
+    """
     if not project_name:
         raise ValueError('Project name cannot be empty')
     project_dir = dir_path / project_name
@@ -52,6 +80,27 @@ def create_project_mods_structure(project_name: str, dir_path: Union[PathLike, P
 
 
 def write_metadata_mods(title: str, author: str, version: Tuple[int, int], category: str, project_dir: Union[PathLike, Path]):
+    """
+    Writes the metadata of a project mods to a file.
+
+    Given a project name, author, version, category, and a directory path, writes the metadata
+    of the project mods to a file. The file will be a JSON file with the name 'METADATA.json'
+    and will be located in the project directory.
+
+    The JSON file will contain the following information:
+    {
+        'name': <project name>,
+        'author': <author>,
+        'version': <version tuple>,
+        'category': <category>
+    }
+
+    :param title: The name of the project
+    :param author: The author of the project
+    :param version: The version of the project
+    :param category: The category of the project
+    :param project_dir: The directory path where the project directory is located
+    """
     metadata_dict = {
         'name': title,
         'author': author,
@@ -64,6 +113,23 @@ def write_metadata_mods(title: str, author: str, version: Tuple[int, int], categ
 
 
 def read_metadata_mods(metadata_file: Union[PathLike, Path]) -> Dict:
+    """
+    Reads the metadata of a project mods from a file.
+
+    Given a file path of a 'METADATA.json' file, reads the metadata of the project mods
+    from the file and returns it as a dictionary. The returned dictionary will contain
+    the following information:
+    {
+        'name': <project name>,
+        'author': <author>,
+        'version': <version tuple>,
+        'category': <category>
+    }
+
+    :param metadata_file: The file path of the 'METADATA.json' file
+    :return: A dictionary containing the metadata of the project mods
+    :raises FileNotFoundError: If the file does not exist
+    """
     if not metadata_file.exists():
         raise FileNotFoundError(f'Metadata file does not exist: {metadata_file}')
     with open(metadata_file, 'r') as f:
@@ -73,12 +139,31 @@ def read_metadata_mods(metadata_file: Union[PathLike, Path]) -> Dict:
 
 
 def write_description_mods(desc: str, project_dir: Union[PathLike, Path]):
+    """
+    Writes a description of a project mods to a file.
+
+    Given a string and a project directory path, writes the string
+    to a file named 'DESCRIPTION.html' in the project directory.
+
+    :param desc: The description of the project mods
+    :param project_dir: The directory path of the project mods directory
+    """
     description_file = project_dir / 'DESCRIPTION.html'
     with open(description_file, 'w') as f:
         f.write(desc)
 
 
 def read_description_mods(desc_file: Union[PathLike, Path]) -> str:
+    """
+    Reads a description of a project mods from a file.
+
+    Given a file path of a 'DESCRIPTION.html' file, reads the description of the project mods
+    from the file and returns it as a string.
+
+    :param desc_file: The file path of the 'DESCRIPTION.html' file
+    :return: A string containing the description of the project mods
+    :raises FileNotFoundError: If the file does not exist
+    """
     if not desc_file.exists():
         raise FileNotFoundError(f'Description file does not exist: {desc_file}')
     with open(desc_file, 'r') as f:
@@ -94,6 +179,21 @@ def create_project_mods(
         category: str,
         description: str
 ) -> None:
+    """
+    Creates a project mods directory.
+
+    Given a directory path, a project name, an author, a version, a category, and a description,
+    creates a project mods directory with the given name in the specified directory.
+    The directory will contain a METADATA.json file with the given information and a DESCRIPTION.html
+    file with the given description.
+
+    :param dir_path: The directory path where the project mods directory will be created
+    :param project_name: The name of the project
+    :param author: The author of the project
+    :param version: The version of the project as a tuple of two integers
+    :param category: The category of the project
+    :param description: The description of the project as a string
+    """
     if not dir_path.exists():
         raise err.InvalidDirectoryPath(f'Directory does not exist: {dir_path}')
 
@@ -106,6 +206,17 @@ def create_project_mods(
 
 @deco.validate_directory
 def is_project_mods_directory(dir_path: Union[PathLike, Path]) -> bool:
+    """
+    Checks if a given directory path is a valid project mods directory.
+
+    A project mods directory should contain a 'modfiles' subdirectory and two files:
+    'METADATA.json' and 'DESCRIPTION.html'. If the directory does not exist, does not
+    contain the 'modfiles' subdirectory, or does not contain the 'METADATA.json' and
+    'DESCRIPTION.html' files, this function will return False.
+
+    :param dir_path: The directory path to check
+    :return: True if the directory path is a valid project mods directory, False otherwise
+    """
     if not dir_path.exists():
         raise err.InvalidDirectoryPath(f'Directory does not exist: {dir_path}')
     # check modfiles subdirectory
@@ -118,6 +229,16 @@ def is_project_mods_directory(dir_path: Union[PathLike, Path]) -> bool:
 
 @deco.validate_directory
 def is_dsdb_directory(dir_path: Union[PathLike, Path]) -> bool:
+    """
+    Checks if a given directory path is a valid DSDB directory.
+
+    A DSDB directory should contain at least 2 '.name' files. If the directory does not exist,
+    does not contain any '.name' files, or does not contain at least 2 '.name' files, this
+    function will return False.
+
+    :param dir_path: The directory path to check
+    :return: True if the directory path is a valid DSDB directory, False otherwise
+    """
     if not dir_path.exists():
         raise err.InvalidDirectoryPath(f'Directory does not exist: {dir_path}')
     # check any .name files
@@ -146,6 +267,24 @@ def copy_asset_file(
         file: str,
         replace: bool = True
 ) -> CopyResult:
+    """
+    Copies a file from a source directory to a destination directory.
+
+    If the destination file already exists, the `replace` parameter determines
+    whether to overwrite the destination file or not. If `replace` is True,
+    the existing destination file is renamed by appending ".old" to the end
+    of the filename. If `replace` is False, a `CopyResult` with a success of
+    False is returned with a message indicating that the destination file
+    already exists.
+
+    If the destination directory does not exist, it is created with parents.
+
+    :param src_dir: The source directory containing the file to copy
+    :param dest_dir: The destination directory to copy the file to
+    :param file: The name of the file to copy
+    :param replace: Whether to overwrite the destination file if it already exists
+    :return: A `CopyResult` indicating the success and details of the copy operation
+    """
     src_path = src_dir / file
     dest_path = dest_dir / file
     dest_old = dest_path.with_name(f'{dest_path.name}.old')
@@ -184,6 +323,11 @@ def copy_asset(
         dest_dir: Union[PathLike, Path],
         replace: bool = True
 ) -> Generator[CopyResult, None, None]:
+    """
+    Copies all files in the given list from their respective source directories to a single destination directory.
+
+    This is a convenience function that simply calls `copy_asset_file` for each file in the list.
+    """
     for src_file in src_files:
         src_dir = src_file.parent
         file_name = src_file.name
@@ -191,6 +335,18 @@ def copy_asset(
 
 
 def pack_project_mods(project_mods_dir: Union[PathLike, Path], dest_dir: Union[PathLike, Path], zip_file_name: str):
+    """
+    Packs all files in the given project mods directory into a ZIP file.
+
+    This function creates a ZIP file at the given destination directory with the given name.
+    The contents of the project mods directory are recursively added to the ZIP file,
+    maintaining their relative directory structure.
+
+    :param project_mods_dir: The project mods directory to pack
+    :param dest_dir: The destination directory to create the ZIP file in
+    :param zip_file_name: The name of the ZIP file to create
+    :raises InvalidProjectModsDirectory: If `project_mods_dir` is not a valid project mods directory
+    """
     if not is_project_mods_directory(project_mods_dir):
         raise err.InvalidProjectModsDirectory(f'Directory is not project mods directory: {project_mods_dir}')
     zip_file_path = dest_dir / zip_file_name
