8ba72b5234246d91bdd7d7df95e3be25901216e7
Alamithor <ahmad.yudhistira.alam@gmail.com>
2024-09-28 20:50:56 +0700

    stop scanning

--- src/DigiSModEditor/threads.py
+++ src/DigiSModEditor/threads.py
@@ -1,28 +1,43 @@
 import os
 import logging
+import time
 
 from PySide6.QtCore import QThread, Signal
 
 from . import core
 from . import constants as const
 
-logger = logging.getLogger(const.LogName.THREAD)
+log = logging.getLogger(const.LogName.THREAD)
 
 
 class ScannerThread(QThread):
-    all_scan_finished = Signal()
+    scan_finished = Signal()
     asset_file_found = Signal(dict)
     data_file_found = Signal(dict)
 
     def __init__(self, dir_path):
         super().__init__()
         self._dir_path = dir_path
+        self._last_scan_time = 0
+        self._stop = False
 
     @property
     def dir_path(self): return self._dir_path
 
+    @property
+    def last_scan_time(self) -> float: return self._last_scan_time
+
+    def stop(self):
+        self._stop = True
+
     def run(self):
+        self._last_scan_time = time.time()
+
         for root, dirs, files in os.walk(self.dir_path):
+            if self._stop:
+                log.info('Stop scanning')
+                break
+
             if files:
                 files_text = ''
                 name_list = [o for o in files if o.endswith('.name')]
@@ -30,13 +45,18 @@ class ScannerThread(QThread):
                     files_text = ';'.join(files)
 
                 for name in name_list:
+                    if self._stop:
+                        log.info('Stop scanning')
+                        break
+
                     asset_files = core.get_asset_related_files(name, files_text)
                     if asset_files:
                         ast_file_path = os.path.join(root, name)
-                        logger.info(f'Found asset file: {ast_file_path}')
+                        log.info(f'Found asset file: {ast_file_path}')
 
                         self.asset_file_found.emit(asset_files)
 
-        self.all_scan_finished.emit()
+        if not self._stop:
+            self.scan_finished.emit()
 
 
